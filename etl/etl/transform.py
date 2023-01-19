3a9d3a7251d4a0dd86a325ac93ad2b8d03d3a4dc
Arthur Lakiza <lakiza@gmail.com>
2023-01-19 16:20:27 +0300

    Чистка от неиспользуемого кода

--- etl/etl/transform.py
+++ etl/etl/transform.py
@@ -2,7 +2,6 @@ import logging
 from collections.abc import Coroutine
 from etl.models import Movie, Person, Genre
 from utils.coroutine import coroutine
-from pydantic import BaseModel
 
 
 class Transform:
@@ -30,28 +29,7 @@ class Transform:
                 target.send(tuple(x[0] for x in res))
 
     @coroutine
-    def transform_movies(self, target: Coroutine[None, list, None]):
-        """Подготовка данных для bulk запроса в Elasticsearch"""
-        while rows := (yield):
-            data = []
-            for row in rows:
-                action = {"index": {"_index": "movies", "_id": row["id"]}}
-                movie = Movie(
-                    id=row["id"],
-                    imdb_rating=row["imdb_rating"] if row["imdb_rating"] else 0,
-                    genre=row["genres"],
-                    title=row["title"],
-                    description=row["description"],
-                    **self.persons_parse(row["persons"]),
-                )
-
-                data.append(action)
-                data.append(movie.dict())
-
-            target.send(data)
-
-    @coroutine
-    def transform_basic(self, index: str, model: Movie | Person | Genre, target: Coroutine[None, list, None]):
+    def transform(self, index: str, model: Movie | Person | Genre, target: Coroutine[None, list, None]):
         """Подготовка данных для bulk запроса в Elasticsearch"""
         while rows := (yield):
             data = []
@@ -63,27 +41,3 @@ class Transform:
                 data.append(_model.dict())
 
             target.send(data)
-
-    def persons_parse(self, persons: list) -> dict:
-        """Парсинг персон кинопроизведения"""
-        res = {
-            "director": [],
-            "actors_names": [],
-            "writers_names": [],
-            "actors": [],
-            "writers": [],
-            "directors": []
-        }
-        for person in persons:
-            match person["role"]:
-                case "director":
-                    res["director"].append(person["name"])
-                    res["directors"].append({"id": person["id"], "name": person["name"]})
-                case "writer":
-                    res["writers_names"].append(person["name"])
-                    res["writers"].append({"id": person["id"], "name": person["name"]})
-                case "actor":
-                    res["actors_names"].append(person["name"])
-                    res["actors"].append({"id": person["id"], "name": person["name"]})
-
-        return res
